979349221c26440c18db2ac2352798eb1f1c187a
VicAcosta90 <Vicacosta90@gmail.com>
2025-05-22 10:05:06 +0200

    act

--- CS50P/hello.py
+++ CS50P/hello.py
@@ -8,11 +8,11 @@
 
 def main():
     name = input("What's your name? ")
-    hello(name)
+    print(hello(name))
 
 
 def hello(to="world"):
-    return f"Hello,", {to}
+    return f"hello, {to}"
 
 
 if __name__ == "__main__":

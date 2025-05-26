b3fe5238913eb29819676cb0d4f2c5557d6b5622
VicAcosta90 <Vicacosta90@gmail.com>
2025-05-26 13:08:46 +0200

    act

--- CS50P/students.py
+++ CS50P/students.py
@@ -1,12 +1,8 @@
 import csv
 
-students = []
+name = input("What's your name? ")
+home = input("Where's your home? ")
 
-with open("students.csv") as file:
-    reader = csv.reader(file)
-    for name, home in reader:
-        students.append({"name": name, "home": home})
-
-
-for student in sorted(students, key=lambda student: student["name"]):  #key=lambda(anonymous function) student(this function is called in everone of the students(dictionaries) in that list) 
-    print(f"{student['name']} is in {student['home']}")               #student:name(i want to recall given a "student" i want to index into that dictionary and access their "name")
+with open("students.csv", "a") as file:
+    writer = csv.DictWriter(file, fieldnames=["name", "home"])
+    writer.writerow({"name": name, "home": home})

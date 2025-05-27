4bb5b3dbc8e8297760ccb62200867ed155c9b6d5
VicAcosta90 <Vicacosta90@gmail.com>
2025-05-27 17:56:34 +0200

    act

--- CS50P/format.py
+++ CS50P/format.py
@@ -1,5 +1,6 @@
+import re
+
 name = input("What's your name? ").strip()
-if "," in name:
-    last, first = name.split(", ")
-    name = f"{first} {last}"
+if matches := re.search(r"^(.+), ?(.+)$", name):
+    name = matches.group(2) + " " + matches.group(1)
 print(f"hello, {name}")

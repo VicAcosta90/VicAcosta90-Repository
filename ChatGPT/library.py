08b347edbfa31ea4ef9e46a5b1fd8e869c6eeb05
VicAcosta90 <Vicacosta90@gmail.com>
2025-06-02 12:03:32 +0200

    act

---
+++ ChatGPT/library.py
@@ -0,0 +1,24 @@
+class Book:
+    def __init__(self, title, author, pages, available=True):
+        self.title = title
+        self.author = author
+        self.pages = pages
+        self.available = True
+
+    def borrow(self):
+        if self.available:
+            self.available = False
+            print(f"You have borrowed {self.title}")
+        else:
+            print(f"Sorry, {self.title} is not available.")
+
+    def return_book(self):
+        self.available = True
+        print(f"You have returned {self.title}")
+
+
+class Library:
+    def __init__(self):
+        list = []
+
+    def add_book(title, author, pages):

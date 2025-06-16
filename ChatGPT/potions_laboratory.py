df834cd980e1ad2694cffce3b5e914609b152cda
VicAcosta90 <Vicacosta90@gmail.com>
2025-06-16 17:49:13 +0200

    act

---
+++ ChatGPT/potions_laboratory.py
@@ -0,0 +1,8 @@
+ingredientes = {
+    "mandrágora": {"poder": 40, "toxicidad": 20},
+    "ojo de tritón": {"poder": 25, "toxicidad": 30},
+    "pluma de fénix": {"poder": 50, "toxicidad": 10},
+    "escama de dragón": {"poder": 70, "toxicidad": 50},
+    "lágrima de unicornio": {"poder": 100, "toxicidad": 5},
+    "hongo lunar": {"poder": 20, "toxicidad": 25}
+}
